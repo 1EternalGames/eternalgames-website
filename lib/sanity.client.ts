b42d0dc7a0d49cd322a303aba7f4811c4b93fc26
1-eyx <mhmfalsaadd@gmail.com>
2025-11-09 21:24:43 +0330

    few4

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -7,11 +7,5 @@ export const client = createClient({
     projectId,
     dataset,
     apiVersion,
-    // THE FIX: useCdn must be false for server-to-server communication to ensure fresh data
-    // and to use the provided token.
-    useCdn: false, 
-    // THE FIX: Provide the read token to authenticate requests from the server.
-    token: process.env.SANITY_API_READ_TOKEN,
-    // Best practice: specify that we want the published version of documents.
-    perspective: 'published',
+    useCdn: true,
 });
